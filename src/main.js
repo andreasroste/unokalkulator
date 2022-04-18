a5358ca63657a827dfb6097fbb9d2bfbd801b7a3
Andreas Røste <andreas.roste@rugge.org>
2022-04-18 20:59:21 +0200

    magien er ekte

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import dayjs from "dayjs";
+
+require("dayjs/locale/nb")
+dayjs.locale("no")
 
 Vue.config.productionTip = false;
 
--- src/views/Statistikk.vue
+++ src/views/Statistikk.vue
@@ -18,11 +18,49 @@
         hide-default-footer
         :items-per-page="-1">
     </v-data-table>
+    <v-card>
+      <v-card-title>Maksimal poengsum</v-card-title>
+      <v-card-text>
+        Som vi vet, slutter alle spill ved 500 poeng eller mer. Her kan du se hvor langt over 500 som er registrert! <br>
+        <b>Spiller:</b> {{max.spiller}} <br>
+        <b>Poeng:</b> {{max.poeng}} <br>
+        <b>Spillnavn:</b> {{max.spillnavn}} <br>
+        <b>Dato:</b> {{max_date}} <br>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text color="primary" @click="loadMaxPoints">Last inn</v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-card>
+      <v-card-title>Spillere</v-card-title>
+      <v-card-text>
+        <v-simple-table>
+          <thead>
+            <tr>
+              <th>Spiller</th>
+              <th>Gruppe</th>
+              <th>Graf</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </v-simple-table>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text color="primary" @click="loadPlayerSparks">Last inn</v-btn>
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 
 <script>
 import axios from "axios";
+import dayjs from "dayjs";
 
 export default {
   data() {
@@ -30,14 +68,26 @@ export default {
       data: [],
       spillere: [],
       data_spiller: {},
+      spark_data: {},
       events: [],
       cal_model: '',
       selectedEvent: '',
       table_spillere_headers: [
         {text: "Navn", align: "left", sortable: true, value: "navn"},
         {text: "Antall spill", align: "left", sortable: true, value: "antall_spill"},
-      ]
+      ],
+      max: {spiller: "<ikke lastet inn>", poeng: "<ikke lastet inn>", spillnavn: "<ikke lastet inn>", dato: "<ikke lastet inn>"}
     };
+  },
+  computed:{
+    max_date() {
+      return dayjs(this.max.dato).locale("nb").format("dddd D. MMMM YYYY")
+    }
+  },
+  filters: {
+    formatdate(date){
+      return dayjs(date).locale("nb").format("dddd D. MMMM YYYY")
+    }
   },
   mounted() {
     let vm = this;
@@ -66,8 +116,8 @@ export default {
       vm.data = data;
 
       vm.events = data.map(spill => {
-        let start = (new Date(spill.dato));
-        let slutt = (new Date(spill.dato)).setHours(start.getHours() + 1);
+        let start = (new Date(spill.dato)).setHours(start.getHours() - 1);
+        let slutt = (new Date(spill.dato));
         return {
           name: spill.navn,
           start,
@@ -81,6 +131,25 @@ export default {
   methods: {
     selectEvent({ event }) {
       this.selectedEvent = event;
+    },
+    loadMaxPoints() {
+      let vm = this;
+      axios.get('/spill.json').then((res) => {
+        let allespill = res.data;
+        let max = {spiller: "<ikke lastet inn>", poeng: 0, spillnavn: "<ikke lastet inn>", dato: "<ikke lastet inn>"};
+        if(!Array.isArray(allespill)) console.error("allespill er ikke array");
+        allespill.map(spill => {
+          if(typeof spill === "object" && spill != null && Object.keys(spill).includes("poengtavle")){
+            spill.poengtavle.map(poeng => {
+              if(poeng.poeng > max.poeng){
+                console.log(poeng);
+                max = {spiller: poeng.navn, poeng: poeng["poeng"], spillnavn: spill.navn, dato: spill.dato};
+              }
+            })
+          }
+        })
+        vm.max = max;
+      });
     }
   }
 };
